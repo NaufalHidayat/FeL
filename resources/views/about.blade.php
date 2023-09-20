4056528137d2286f8deee7ff5b7a7447e0198354
NaufalHidayat <90307751+NaufalHidayat@users.noreply.github.com>
2023-09-20 09:22:45 +0800

    Membuat Model

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,7 +5,7 @@
 <h1>Halaman About</h1>
 <h1> {{ $name }} </h1>
 <h3> {{ $email }}</h3>
-<img src="img/{{ $image }}" alt="">
+<img src="img/{{ $image }}" alt="" width="200" class="img-thumbnail rounded-circle">
     
 @endsection
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,59 +29,18 @@ Route::get('/about', function () {
     ]);
 });
 
-
-
-
 Route::get('/blog', function () {
-    $project = [
-        [
-            "title" => "Project Pertama",
-            "slug" => "project-pertama",
-            "Language" => "C++",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit non obcaecati optio rem magnam deserunt blanditiis a voluptatum et veritatis quos corrupti labore doloribus, quisquam quibusdam? Porro voluptates voluptatem enim? Unde, vitae ad quibusdam enim atque repellat accusantium dolores laudantium explicabo, corporis beatae, ipsum consequuntur quam rerum itaque quos qui? Eveniet sit impedit quas laudantium tempora eum nesciunt, velit officiis facere numquam officia iusto veritatis cumque aperiam, ea quis quos totam magni possimus error ducimus qui quod? Qui, natus officiis." 
-        ],
-        [
-            "title" => "Project Kedua",
-            "slug" => "project-kedua",
-            "Language" => "SolidJS",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit consequuntur quae nisi! Amet vitae illo repellat quis, unde quod rerum debitis eos placeat alias ipsa sed aut esse, est cumque ut ex at laborum! Porro temporibus ducimus quaerat aperiam possimus soluta ea dolorem qui praesentium eligendi, repellendus error officiis vitae!" 
-        ]
-        ];
-        
-
     return view('posts', [
         "title" => "Blog",
-        "posts" => $project
+        "posts" => Post::all()
     ]);
 });
 
 // halaman single post
 
 Route::get('posts/{slug}', function($slug){
-    $project = [
-        [
-            "title" => "Project Pertama",
-            "slug" => "project-pertama",
-            "Language" => "C++",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit non obcaecati optio rem magnam deserunt blanditiis a voluptatum et veritatis quos corrupti labore doloribus, quisquam quibusdam? Porro voluptates voluptatem enim? Unde, vitae ad quibusdam enim atque repellat accusantium dolores laudantium explicabo, corporis beatae, ipsum consequuntur quam rerum itaque quos qui? Eveniet sit impedit quas laudantium tempora eum nesciunt, velit officiis facere numquam officia iusto veritatis cumque aperiam, ea quis quos totam magni possimus error ducimus qui quod? Qui, natus officiis." 
-        ],
-        [
-            "title" => "Project Kedua",
-            "slug" => "project-kedua",
-            "Language" => "SolidJS",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit consequuntur quae nisi! Amet vitae illo repellat quis, unde quod rerum debitis eos placeat alias ipsa sed aut esse, est cumque ut ex at laborum! Porro temporibus ducimus quaerat aperiam possimus soluta ea dolorem qui praesentium eligendi, repellendus error officiis vitae!" 
-        ]
-        ];
-
-    $new_post = [];
-        foreach($project as $post){
-            if($post["slug"] === $slug){
-                $new_post = $post ;
-            }
-        }
-
     return view('post', [
         "title" => "single post",
-        "post" => $new_post
+        "post" => Post::find($slug)
     ]);
 });
