899799dda753769616f1ceaf09eaf6f30cb36d01
NaufalHidayat <90307751+NaufalHidayat@users.noreply.github.com>
2023-09-24 08:30:46 +0700

    Membuat Factory

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,7 +4,7 @@
 @section('container')
 <h2>{{ $post["title"] }}</h2>
 
-<p><a href="" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+<p><a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
 
 {!! $post["body"] !!}
     
